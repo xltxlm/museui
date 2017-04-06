62d46df46d7994d4f27a39e5cf34647d90647bd2
夏琳泰 <xltxlm@qq.com>
2017-04-06 19:35:43 +0800

    * 2017-04-06 初始化项目

---
+++ src/Traits/Header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialintai
+ * Date: 2017/4/6
+ * Time: 19:26
+ */
+
+namespace xltxlm\museui\Traits;
+
+
+class Header
+{
+
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import 'muse-ui/dist/theme-light.css' // 使用 carbon 主题
+Vue.use(MuseUI)
